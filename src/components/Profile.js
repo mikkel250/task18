6c2d8b079d02df18fc9098988bd07abae97a38f6
Mikkel Ridley <mikkel250@gmail.com>
2019-06-03 22:27:08 -0700

    fix state handlers to show correct input in each field

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -5,15 +5,16 @@ class Profile extends React.Component {
         super(props);
         this.state = {
             name: '',
-            email: "",
-            address: ""
+            email: '',
+            address: ''
         };
+
         this.handleChange = this.handleChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
     }
 
     handleChange(event) {
-        this.setState({ value: event.target.value });
+        this.setState({ [event.target.value]: event.target.value });
     }
 
     handleSubmit(event) {
@@ -24,18 +25,18 @@ class Profile extends React.Component {
     render() {
         return (
             <div class="row">
-            <form class="col s3"> Edit Your Profile: <br /> 
-                <label>Name:
-                <input type='text' value={this.state.value} onChange={this.handleChange} name="name"></input><br />                 
-                </label>
-                <label>Email:
-                <input type='text' value={this.state.value} onChange={this.handleChange} name="email"></input><br /> 
-                </label>
-                <label>Address:
-                <input type='text' value={this.state.value} onChange={this.handleChange} name="address"></input><br /> 
-                </label>
-                <button class="btn waves-effect waves-light" type="submit" name="action" onClick={this.handleSubmit}>Submit<i class="material-icons right">>></i>
-                </button>
+                <form class="col s3"> Edit Your Profile: <br />
+                    <label>Name:
+                <input type='text' onChange={this.handleNameChange} ></input><br />
+                    </label>
+                    <label>Email:
+                <input type='text' onChange={this.handleEmailChange} ></input><br />
+                    </label>
+                    <label>Address:
+                <input type='text' onChange={this.handleAddressChange} ></input><br />
+                    </label>
+                    <button class="btn waves-effect waves-light" type="submit" name="action">Submit<i class="material-icons right">>></i>
+                    </button>
                 </form>
             </div>
         )
